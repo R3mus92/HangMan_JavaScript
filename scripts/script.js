25198c2f1b3ca3b3fdb0eda23af78ee5edc1429b
Remus92 <remusbalaj@yahoo.com>
2024-03-31 17:52:16 +0200

    Hangman Game first commit, 31.03.2024

---
+++ scripts/script.js
@@ -0,0 +1,91 @@
+// Selecting elements from the page(Variables)
+const wordDisplay = document.querySelector(".word-display");
+const guessesText = document.querySelector(".guesses-text b");
+const keyboardDiv = document.querySelector(".keyboard");
+const hangmanImage = document.querySelector(".hangman-box img");
+const gameModal = document.querySelector(".game-modal");
+const playAgainBtn = gameModal.querySelector(".play-again");
+
+// Initialize the game variables
+let currentWord, correctLetters, wrongGuessCount;
+const maxGuessess = 6;
+
+const resetGame = () => {
+   correctLetters = [];
+   wrongGuessCount = 0;
+   hangmanImage.src = "images/hangman-0.svg";
+   guessesText.innerText = `${wrongGuessCount} / ${maxGuessess}`;
+   // Create the empty letter slots
+   wordDisplay.innerHTML = currentWord.split("").map(() => `<li class="letter"></li>`).join("");
+   // Enable keyboard buttons
+   keyboardDiv.querySelectorAll("button").forEach(btn => btn.disabled = false);
+   // Hide the game modal
+   gameModal.classList.remove("show");
+}
+
+// Function to get a random word and set up the game
+const getRandomWord  = () => {
+   // Picking a random word and hint from your wordList array
+   const { cuvant, sugestie } = wordList[Math.floor(Math.random() * wordList.length)];
+   // Set the current word and update the hint
+   currentWord = cuvant;
+   document.querySelector(".hint-text b").innerText = sugestie;
+   // Call reset game
+   resetGame();
+}
+
+// Function to handle the end of game win or lose
+const gameOver = (isVictory) => {
+   // Show the game over modal with win or loss
+   const modalText = isVictory ? `Ai gasit cuvantul:` : 'Cuvantul corect a fost:';
+   gameModal.querySelector("img").src = `images/${isVictory ? 'victory' : 'lost'}.gif`;
+   gameModal.querySelector("h4").innerText = isVictory ? 'Felicitari' : 'Jocul s-a terminat!';
+   gameModal.querySelector("p").innerHTML = `${modalText} <b>${currentWord}</b>`;
+   gameModal.classList.add("show");
+}
+
+// Creating a for loop to display our keyboard buttons
+
+for (let i = 97; i <= 122; i++) {
+   const button = document.createElement("button");
+   button.innerText = String.fromCharCode(i);
+   keyboardDiv.appendChild(button);
+   // Adding a click event listener for each button
+   button.addEventListener("click", (e) => initGame(e.target, String.fromCharCode(i)));
+}
+
+// Function to handle the game logic when a letter is clicked
+
+const initGame = (button, clickedLetter) => {
+   // Checking if the clicked letter is in the current word
+   if (currentWord.includes(clickedLetter)) {
+      // Update the displayed letters if clicked is correct
+      [...currentWord].forEach((letter, index) =>{
+         if (letter === clickedLetter) {
+            correctLetters.push(letter);
+            wordDisplay.querySelectorAll("li")[index].innerText = letter;
+            wordDisplay.querySelectorAll("li")[index].classList.add("Guessed");
+         }
+      });
+   } else {
+      // update wrong guess count and hangman image if letter is incorrect
+      wrongGuessCount++;
+      hangmanImage.src = `images/hangman-${wrongGuessCount}.svg`;
+   }
+   // disable the clicked button so it can't be clicked again
+   button.disabled = true;
+   // update the displayed guess count
+   guessesText.innerText = `${wrongGuessCount} / ${maxGuessess}`;
+
+   // check if the game should end based on win or lose conditions
+   if (wrongGuessCount === maxGuessess) return gameOver(false);
+   if (correctLetters.length === currentWord.length) return gameOver(true);
+
+
+}
+
+// Starting the game with a random word
+getRandomWord();
+
+// Add event listener for the play again button
+playAgainBtn.addEventListener("click", getRandomWord);
---
+++ scripts/word-list.js
@@ -0,0 +1,142 @@
+const wordList = [
+    {
+        cuvant: "pinguin",
+        sugestie: "Un pasăre care nu poate zbura și care iubește frigul."
+    },
+    {
+        cuvant: "banana",
+        sugestie: "Un fruct galben pe care îl iubesc maimuțele."
+    },
+    {
+        cuvant: "guma de mestecat",
+        sugestie: "Elastică și in diferite culori, iubita de copii."
+    },
+    {
+        cuvant: "insorit",
+        sugestie: "Vremea când cerul este senin și luminos."
+    },
+    {
+        cuvant: "robot",
+        sugestie: "Un prieten mecanizat care urmează comenzi."
+    },
+    {
+        cuvant: "supererou",
+        sugestie: "Un personaj fictiv cu puteri extraordinare."
+    },
+    {
+        cuvant: "prajitura",
+        sugestie: "Un desert delicios adesea acoperit cu glazură."
+    },
+    {
+        cuvant: "ras",
+        sugestie: "Sunetul vesel al amuzamentului."
+    },
+    {
+        cuvant: "zmeu",
+        sugestie: "Un jucărie zburătoare atașată de un șnur."
+    },
+    {
+        cuvant: "scanteiere",
+        sugestie: "Luciu cu mici sclipiri de lumină."
+    },
+    {
+        cuvant: "confortabil",
+        sugestie: "Loc relaxant, potrivit pentru odihna"
+    },
+    {
+        cuvant: "fluier",
+        sugestie: "Un dispozitiv care face un sunet cand sufli in el."
+    },
+    {
+        cuvant: "puzzle",
+        sugestie: "Un joc sau provocare pentru a-ți testa creierul construit din multe piese."
+    },
+    {
+        cuvant: "licurici",
+        sugestie: "Un insect mic care luminează în întuneric."
+    },
+    {
+        cuvant: "foc de tabara",
+        sugestie: "Un foc cald în aer liber pentru gătit sau adunare."
+    },
+    {
+        cuvant: "vrajitor",
+        sugestie: "O persoană magică cu puteri speciale."
+    },
+    {
+        cuvant: "margareta",
+        sugestie: "O floare albă simplă și veselă."
+    },
+    {
+        cuvant: "jungla",
+        sugestie: "O pădure densă și tropicală plină de viață sălbatică."
+    },
+    {
+        cuvant: "briosa",
+        sugestie: "O pâine dulce adesea umpluta cu fructe sau nuci."
+    },
+    {
+        cuvant: "perna",
+        sugestie: "Moale și pufoasă pentru a-ți odihni capul."
+    },
+    {
+        cuvant: "instelata",
+        sugestie: "Plină de stele strălucitoare și luminoase."
+    },
+    {
+        cuvant: "jucaus",
+        sugestie: "Plin de distracție și activitate plină de viață."
+    },
+    {
+        cuvant: "magie",
+        sugestie: "Forțe supranaturale misterioase și fermecătoare."
+    },
+    {
+        cuvant: "rasete",
+        sugestie: "Sunete emanate atunci cand esti fericit."
+    },
+    {
+        cuvant: "soare",
+        sugestie: "Corp ceresc care incalzeste pamantul."
+    },
+    {
+        cuvant: "soapta",
+        sugestie: "A vorbi foarte încet sau liniștit, adesea într-un mod secretos."
+    },
+    {
+        cuvant: "teren de joaca",
+        sugestie: "O zonă în aer liber cu echipamente pentru copii de a se juca."
+    },
+    {
+        cuvant: "acadea",
+        sugestie: "Un dulce pe un băț, adesea colorat în culori aprinse."
+    },
+    {
+        cuvant: "girafa",
+        sugestie: "Un mamifer african înalt cu gât lung."
+    },
+    {
+        cuvant: "bula",
+        sugestie: "O sferă subțire de lichid care înclude aer sau gaz."
+    },
+    {
+        cuvant: "desene animate",
+        sugestie: "Un film sau emisiune TV animat cu personaje amuzante."
+    },
+    {
+        cuvant: "zambet",
+        sugestie: "O expresie facială fericită."
+    },
+    {
+        cuvant: "presarat",
+        sugestie: "Bucățele mici de bomboane sau decorațiuni ori fel de a condimenta mancarea."
+    },
+    {
+        cuvant: "pufoasa",
+        sugestie: "Moale si matasoasa."
+    },
+    {
+        cuvant: "ghinda",
+        sugestie: "Fructul unui stejar."
+    }
+];
